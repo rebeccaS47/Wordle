c7e88a467919b3697f14834e701dbad941c986cc
Ting Hsuan Shen <rebecca4122031220@gmail.com>
2024-08-21 02:50:02 +0800

    add change bgcolor when press enter

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const ans = 'RIGHT';
 const initialState = {
   array: Array(rows)
     .fill()
-    .map(() => Array(cols).fill('')),
+    .map(() => Array(cols).fill(' ')),
   nowRow: 0,
   nowCol: 0,
 };
@@ -27,7 +27,7 @@ function reducer(state, action) {
     case 'REMOVE_LETTER': {
       if (state.nowCol <= 0) return state;
       const newArray = [...state.array];
-      newArray[state.nowRow][state.nowCol - 1] = '';
+      newArray[state.nowRow][state.nowCol - 1] = ' ';
       return {
         ...state,
         array: newArray,
@@ -35,25 +35,33 @@ function reducer(state, action) {
       };
     }
     case 'SUBMIT_GUESS': {
-      if (state.nowCol < cols) {
-        return state;
-      } else {
-        if (
-          state.array[state.nowRow].join('') === ans ||
-          state.nowRow >= rows
-        ) {
-          return state;
-        }
-        return {
-          ...state,
-          nowRow: state.nowRow + 1,
-          nowCol: 0,
-        };
-      }
+      if (state.nowCol < cols) return state;
+
+      // if (state.array[state.nowRow].join('') === ans || state.nowRow >= rows) {
+      //   return state;
+      // }
+      return {
+        ...state,
+        nowRow: state.nowRow + 1,
+        nowCol: 0,
+      };
     }
     default:
       return state;
   }
+}
+
+function getColor(letter, i, j, row) {
+  if (i < row) {
+    if (letter === ans[j]) {
+      return 'bg-green-500';
+    } else if (ans.includes(letter)) {
+      return 'bg-yellow-500';
+    } else {
+      return 'bg-gray-500';
+    }
+  }
+  return 'bg-white';
 }
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -92,14 +100,17 @@ function App() {
     <div className="flex flex-col w-[350px] mx-auto mt-5">
       {state.array.map((row, i) => (
         <div key={i} className="flex justify-between mb-2">
-          {row.map((cell, j) => (
-            <div
-              key={j}
-              className="flex items-center justify-center border border-gray-300 w-[62px] h-[62px] text-base"
-            >
-              {cell}
-            </div>
-          ))}
+          {row.map((cell, j) => {
+            const bgcolor = getColor(cell, i, j, state.nowRow);
+            return (
+              <div
+                key={j}
+                className={`flex items-center justify-center border border-gray-300 w-[62px] h-[62px] text-base ${bgcolor}`}
+              >
+                {cell}
+              </div>
+            );
+          })}
         </div>
       ))}
     </div>
